6cfdb88852f0b2242ede5f16812b0d8b191ab0d3
maherodeh69 <77062070+maherodeh69@users.noreply.github.com>
2024-03-27 18:55:58 +0200

    Delete lib directory

--- lib/auth_notification/firebase_api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-
-Future<String?> initNotifications() async { // getting token device for enabling app notification
-  final firebaseMessaging = FirebaseMessaging.instance;
-  await firebaseMessaging.requestPermission();
-  final fcmToken = await firebaseMessaging.getToken();
-  return fcmToken;
-}
